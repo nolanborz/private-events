57b5559b57d11e0310f00ffaf93195dc816e95ae
nolanborz <nolanrborzoni@gmail.com>
2024-11-22 15:18:37 +0900

    Connect events and users, adjust routes, make views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
-
+  resources :users, only: [ :show ]
   root "events#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
