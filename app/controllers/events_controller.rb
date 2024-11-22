eeb7584b943129d0287d0b996e4e1e3e1e65a0bd
nolanborz <nolanrborzoni@gmail.com>
2024-11-22 15:39:59 +0900

    Make event creation form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,11 +7,17 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
-
   def create
-  end
+    @event = current_user.created_events.build(event_params)
 
+    if @event.save
+      redirect_to @events
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
   def edit
   end
 
@@ -19,5 +25,11 @@ class EventsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date)
   end
 end
