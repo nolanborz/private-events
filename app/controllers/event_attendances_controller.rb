d6009fa7ab84370676dcb98a52a00c9e5d14cbd9
nolanborz <nolanrborzoni@gmail.com>
2024-11-24 15:41:06 +0900

    Fix attend and quit event buttons

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,12 +1,12 @@
 class EventAttendancesController < ApplicationController
   def create
-    event = Event.find(params[:event_id])
+    event = Event.find(params[:id])
     current_user.attended_events << event
     redirect_to event_path(event), notice: "You're now registered for this event!"
   end
 
   def destroy
-    event = Event.find(params[:event_id])
+    event = Event.find(params[:id])
     current_user.attended_events.delete(event)
     redirect_to event_path(event), notice: "You're no longer attending this event."
   end
